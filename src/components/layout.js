7011e67ce0fd646635e8755e9e8c7329e0ad5ebc
MariahAdams <mariah.adams23@yahoo.com>
2018-09-15 13:22:55 -0700

    adding static query

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { css } from 'react-emotion';
-import { Link } from "gatsby";
+import { Link, StaticQuery, graphql } from "gatsby";
 import { rhythm } from '../utils/typography';
 
 const ListLink = props => (
@@ -23,27 +23,41 @@ export default ({ children }) => (
     </header>
 
     <main>
-      <div
-        className={css`
-          margin: 0 auto;
-          max-width: 700px;
-          padding: ${rhythm(2)};
-          padding-top: ${rhythm(1.5)};
-        `}
-      >
-        <Link to={`/`}>
-          <h3
-            className={css`
-              margin-bottom: ${rhythm(2)};
-              display: inline-block;
-              font-style: normal;
-            `}
-          >
-            Pandas Eating Lots
-          </h3>
-        </Link>
-        {children}
-      </div>
+    <StaticQuery
+      query={graphql`
+        query {
+          site {
+            siteMetadata {
+              title
+            }
+          }
+        }
+      `
+      }
+      render={data => (
+          <div
+          className={css`
+            margin: 0 auto;
+            max-width: 700px;
+            padding: ${rhythm(2)};
+            padding-top: ${rhythm(1.5)};
+          `}
+        >
+          <Link to={`/`}>
+            <h3
+              className={css`
+                margin-bottom: ${rhythm(2)};
+                display: inline-block;
+                font-style: normal;
+              `}
+            >
+              {data.site.siteMetadata.title}
+            </h3>
+          </Link>
+          {children}
+        </div>
+      )}
+    />
     </main>
 
     <footer>

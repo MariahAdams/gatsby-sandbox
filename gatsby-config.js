1ee0d0af2d106b61fd28399325485301572b3c16
MariahAdams <mariah.adams23@yahoo.com>
2018-09-15 11:46:51 -0700

    adding in css modules

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,12 @@
 
 
 module.exports = {
-  plugins: [`gatsby-plugin-typography`]
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`
+      }
+    }
+  ]
 }
---
+++ src/pages/about-css-modules.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Container from '../components/container';
+import Header from '../components/header';
+
+export default () => (
+  <Container>
+    <Header headerText="About CSS Modules"/>
+    <p>Hello CSS Modules.</p>
+  </Container>
+)

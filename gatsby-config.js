23e4aa1048d708240dcf1cf37e02f14451bb573a
MariahAdams <mariah.adams23@yahoo.com>
2018-09-15 13:49:28 -0700

    renamed files, updated config and package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
